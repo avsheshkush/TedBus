212c40015b42628723f2334d03b912bedf656f45
Avshesh Kushwaha <avsheshkush@gmail.com>
2026-09-22 13:57:24 +0530

    Refactor reportLimiter to use ipKeyGenerator

--- tedbus-backend/src/middleware/reportLimiter.js
+++ tedbus-backend/src/middleware/reportLimiter.js
@@ -1,16 +1,21 @@
-const rateLimit = require("express-rate-limit");
+const { rateLimit, ipKeyGenerator } = require("express-rate-limit");
 
 const reportLimiter = rateLimit({
-  windowMs: 60 * 60 * 1000, 
-  max: 5, 
+  windowMs: 60 * 60 * 1000,
+  max: 5,
+
   message: {
     success: false,
     message: "Too many reports from this account, please try again after an hour.",
   },
+
   standardHeaders: true,
   legacyHeaders: false,
-   keyGenerator: (req) => {
-    return req.user ? req.user._id.toString() : req.ip;
+
+  keyGenerator: (req) => {
+    return req.user
+      ? req.user._id.toString()
+      : ipKeyGenerator(req);
   },
 });
 
